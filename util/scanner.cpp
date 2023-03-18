acc6b9d4ef5d27b26e4a980af3b0b0301b7b9e8e
Samuel Theofie <55137379+BlazedEngima@users.noreply.github.com>
2023-03-18 18:14:16 +0000

    Scanner ver1.0

--- util/scanner.cpp
+++ util/scanner.cpp
@@ -1,5 +1,61 @@
 #include "scanner.hpp"
 
+std::vector<std::string> reserved_words {
+    "int", "main", "void", "break",
+    "do", "else", "if", "while", 
+    "return", "scanf", "printf"
+};
+
+std::ostream & operator<<(std::ostream& out, const Tokens token) {
+    const char* idx = 0;
+
+    #define process_token(p) case p: idx = #p; break;
+    switch (token) {
+        process_token(ID);
+        process_token(INT_NUM);
+        process_token(LBRACE);
+        process_token(RBRACE);
+        process_token(LSQUARE);
+        process_token(RSQUARE);
+        process_token(LPAR);
+        process_token(RPAR);
+        process_token(SEMI);
+        process_token(PLUS);
+        process_token(MINUS);
+        process_token(MUL_OP);
+        process_token(DIV_OP);
+        process_token(AND_OP);
+        process_token(OR_OP);
+        process_token(NOT_OP);
+        process_token(ASSIGN);
+        process_token(LT);
+        process_token(GT);
+        process_token(SHL_OP);
+        process_token(SHR_OP);
+        process_token(EQ);
+        process_token(NOTEQ);
+        process_token(LTEQ);
+        process_token(GTEQ);
+        process_token(ANDAND);
+        process_token(OROR);
+        process_token(COMMA);
+        process_token(INT);
+        process_token(MAIN);
+        process_token(VOID);
+        process_token(BREAK);
+        process_token(DO);
+        process_token(ELSE);
+        process_token(IF);
+        process_token(WHILE);
+        process_token(RETURN);
+        process_token(READ);
+        process_token(WRITE);
+    }
+    #undef process_token
+
+    return out << idx;
+}
+
 Scanner::Scanner(std::string file_name) {
     try {
         this->in_file.open(file_name, std::ios::in);
@@ -10,78 +66,149 @@ Scanner::Scanner(std::string file_name) {
     }
 }
 
-void Scanner::read_int() {
-    this->in_file.get(this->next_char);
-
+void Scanner::print() {
+    for (auto & elem: this->tokens) {
+        std::cout << "Token: " << elem << std::endl;
+    }
 }
 
-void Scanner::read_string() {
+Tokens Scanner::read_string() {
+    this->char_buffer += this->next_char;
 
+    while (isalnum(this->in_file.peek()) || this->in_file.peek() == '_') {
+        this->in_file >> this->next_char;
+        this->char_buffer += this->next_char;  
+    }
+    
+    for (size_t i = 0; i < reserved_words.size(); i++) {
+        if (this->char_buffer == reserved_words[i]) {
+            this->char_buffer = "";
+            return static_cast<Tokens>(28 + i);
+        }
+    }
+    
+    this->char_buffer = "";
+
+    return ID;
+}
+
+Tokens Scanner::special_symbol() {
+    switch (this->next_char) {
+        case '{':
+            return LBRACE;
+            break;
+        case '}':
+            return RBRACE;
+            break;
+        case '[':
+            return LSQUARE;
+            break;
+        case ']':
+            return RSQUARE;
+            break;
+        case '(':
+            return LPAR;
+            break;
+        case ')':
+            return RPAR;
+            break;
+        case ';':
+            return SEMI;
+            break;
+        case '+':
+            return PLUS;
+            break;
+        case '-':
+            return MINUS;
+            break;
+        case '*':
+            return MUL_OP;
+            break;
+        case '/':
+            return DIV_OP;
+            break;
+        case '&': {
+            if (this->in_file.peek() == '&') {
+                this->in_file >> this->next_char;
+                return ANDAND;
+            }
+
+            return AND_OP;
+            break;
+        }
+        case '|': {
+            if (this->in_file.peek() == '|') {
+                this->in_file >> this->next_char;
+                return OROR;
+            }
+
+            return OR_OP;
+            break;
+        }
+        case '!': {
+            if (this->in_file.peek() == '=') {
+                this->in_file >> this->next_char;
+                return NOTEQ;
+            }
+
+            return NOT_OP;
+            break;
+        }
+        case '=': {
+            if (this->in_file.peek() == '=') {
+                this->in_file >> this->next_char;
+                return EQ;
+            }
+
+            return ASSIGN;
+            break;
+        }
+        case '<': {
+            if (this->in_file.peek() == '<') {
+                this->in_file >> this->next_char;
+                return SHL_OP;
+            }
+            else if (this->in_file.peek() == '=') {
+                this->in_file >> this->next_char;
+                return LTEQ;
+            }
+
+            return LT;
+            break;
+        }
+        case '>': {
+            if (this->in_file.peek() == '>') {
+                this->in_file >> this->next_char;
+                return SHR_OP;
+            }
+            else if (this->in_file.peek() == '=') {
+                this->in_file >> this->next_char;
+                return GTEQ;
+            }
+
+            return GT;
+            break;
+        }
+        case ',':
+            return COMMA;
+            break;
+        
+        default:
+            break;
+    }
 }
 
 void Scanner::scan() {
-    while(next_char != EOF) {
-        this->in_file.get(this->next_char);
+    while(this->in_file >> this->next_char) {
 
-        switch (this->next_char) {
-            case '{':
-                this->tokens.push_back(LBRACE);
-                break;
-            case '}':
-                this->tokens.push_back(RBRACE);
-                break;
-            case '[':
-                this->tokens.push_back(LSQUARE);
-                break;
-            case ']':
-                this->tokens.push_back(RSQUARE);
-                break;
-            case '(':
-                this->tokens.push_back(LPAR);
-                break;
-            case ')':
-                this->tokens.push_back(RPAR);
-                break;
-            case ';':
-                this->tokens.push_back(SEMI);
-                break;
-            case '+':
-                this->tokens.push_back(PLUS);
-                break;
-            case '-':
-                this->tokens.push_back(MINUS);
-                break;
-            case '*':
-                this->tokens.push_back(MUL_OP);
-                break;
-            case '/':
-                this->tokens.push_back(DIV_OP);
-                break;
-            case '&':
-                this->tokens.push_back(AND_OP);
-                break;
-            case '|':
-                this->tokens.push_back(OR_OP);
-                break;
-            case '!':
-                this->tokens.push_back(NOT_OP);
-                break;
-            case '=':
-                this->tokens.push_back(ASSIGN);
-                break;
-            case '<':
-                this->tokens.push_back(LT);
-                break;
-            case '>':
-                this->tokens.push_back(GT);
-                break;
-            case ',':
-                this->tokens.push_back(COMMA);
-                break;
-            
-            default:
-                break;
+        if (isalpha(this->next_char)) {
+            this->tokens.push_back(this->read_string());
         }
-    }
 
+        else if (isdigit(this->next_char)) {
+            this->tokens.push_back(INT_NUM);
+        }
+
+        else {this->tokens.push_back(this->special_symbol());}
+    }
 }
--- util/scanner.hpp
+++ util/scanner.hpp
@@ -6,12 +6,6 @@
 #include <unordered_map>
 #include <fstream>
 
-std::vector<std::string> reserved_words {
-    "int", "main", "void", "break",
-    "do", "else", "if", "while", 
-    "return", "scanf", "printf"
-};
-
 typedef enum Tokens {
     ID, INT_NUM, LBRACE, RBRACE, LSQUARE, RSQUARE,
     LPAR, RPAR, SEMI, PLUS, MINUS, MUL_OP, DIV_OP,
@@ -22,67 +16,33 @@ typedef enum Tokens {
 
 } token;
 
-std::ostream & operator<<(std::ostream& out, const Tokens token) {
-    const char* idx = 0;
-
-    #define process_token(p) case p: idx = #p; break;
-    switch (token) {
-        process_token(ID);
-        process_token(INT_NUM);
-        process_token(LBRACE);
-        process_token(RBRACE);
-        process_token(LSQUARE);
-        process_token(RSQUARE);
-        process_token(LPAR);
-        process_token(RPAR);
-        process_token(SEMI);
-        process_token(PLUS);
-        process_token(MINUS);
-        process_token(MUL_OP);
-        process_token(DIV_OP);
-        process_token(AND_OP);
-        process_token(OR_OP);
-        process_token(NOT_OP);
-        process_token(ASSIGN);
-        process_token(LT);
-        process_token(GT);
-        process_token(SHL_OP);
-        process_token(SHR_OP);
-        process_token(EQ);
-        process_token(NOTEQ);
-        process_token(LTEQ);
-        process_token(GTEQ);
-        process_token(ANDAND);
-        process_token(OROR);
-        process_token(COMMA);
-    }
-    #undef process_token
-
-    return out << idx;
-}
-
 class Scanner {
     private:
         std::ifstream in_file;
         char next_char;
         std::vector<Tokens> tokens;
+        std::string char_buffer;
 
-        void read_int();
-        void read_string();
+        Tokens read_int();
+        Tokens read_string();
+        Tokens special_symbol();
 
     public:
         // Ban copying and assignment
         Scanner(Scanner const &) = delete;
         Scanner & operator=(Scanner const &) = delete;
 
-        // Get next token
+        // Get scans file and puts token into tokens vector
         void scan();
+
+        // Prints out token vector
+        void print();
 
         // Constructor
         Scanner(std::string file_name);
 
         // Destructor
-        ~Scanner();
+        // ~Scanner();
 
 };
 
