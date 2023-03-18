c0a912d67bb8919496a5ef0619b43cd640bf10b4
Samuel Theofie <55137379+BlazedEngima@users.noreply.github.com>
2023-03-18 21:30:42 +0000

    Added command line args

--- main.cpp
+++ main.cpp
@@ -1,17 +1,28 @@
 #include "util/scanner.hpp"
 
+// Main function
 int main(int argc, char const *argv[]) {
+    std::vector<Token> tokens {}; // vector that stores input file tokens
+
     if (argc < 2) {
         std::cout << "Please give input file" << std::endl;
         return 0;
     }
-
-    std::string output_file = "./testcases/result/ans";
-    for (int i = 1; i <= argc - 1; i++) {
-        Scanner scanner(argv[i]);
+    // Writes the output tokens to std::cout and to a file called ans.c1
+    else if (argc == 2) {
+        Scanner scanner(argv[1], &tokens);
         scanner.scan();
-        scanner.print(output_file + std::to_string(i) + ".c1");
+        scanner.print("./testcases/result/ans.c1");
+    } 
+    // Writes the output tokens to file
+    else {
+        std::string output_file = "./testcases/result/ans";
+        for (int i = 1; i <= argc - 1; i++) {
+            Scanner scanner(argv[i], &tokens);
+            scanner.scan();
+            scanner.print(output_file + std::to_string(i) + ".c1");
+            tokens.clear();
+        }
     }
-
     return 0;
 }
